31f88834b18c96023130207daa60055ff33868af
lucagump <l.martinelli@hotmail.com>
2017-12-14 02:42:18 +0100

    Arduino-PC

--- Muretto/Serial_RGB_Qt/main.cpp
+++ Muretto/Serial_RGB_Qt/main.cpp
@@ -1,10 +1,12 @@
-#include "mainwindow.h"
+#include "dialog.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Dialog w;
+    w.setFixedSize(560, 160);
+    w.setWindowTitle("RGB LED Controller");
     w.show();
 
     return a.exec();
