eb57cb1a057267f8b45f1e9f6e3c587866222a8b
Thomas <thomasdemin16@gmail.com>
2019-12-03 17:15:23 +0100

    Remove useless files

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@
 # Here you can find the variables get it from Qt Creator
 
 #QMAKE="/home/$USER/Qt/5.10.1/gcc_64/bin/qmake"
-QMAKE="/home/$USER/Qt/5.12.5/gcc_64/bin/qmake"
+#QMAKE="/home/$USER/Qt/5.12.5/gcc_64/bin/qmake"
+QMAKE="/home/$USER/Qt/5.9.8/gcc_64/bin/qmake"
 
 QT_PROJECT="eagleSteeringWheel.pro"
 BUILD_DIR=build/
